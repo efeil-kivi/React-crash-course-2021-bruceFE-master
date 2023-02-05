1380edd80cce26bfedcfbe5f26763048e74d50ca
efeil-kivi <48588983+efeil-kivi@users.noreply.github.com>
2023-02-05 23:41:59 +0800

    完成7，自定义hook&userDebounce

--- jira/src/screens/project-list/index.jsx
+++ jira/src/screens/project-list/index.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
-import {cleanObject} from "../../utils";
+import {cleanObject, useMount} from "utils";
 const apiUrl = process.env.REACT_APP_API_URL;
 import qs from "qs";
+import {useDebounce} from "utils";
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
   const [param, setParam] = useState({
@@ -13,20 +14,21 @@ export const ProjectListScreen = () => {
   });
   const [list, setList] = useState([]);
 
+  const debouncedParam = useDebounce(param, 2000) //每次param变化都会运行
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async (response) => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async (response) => {
       if (response.ok) {
         setList(await response.json());
       }
     });
-  }, [param]);
-  useEffect(() => {
+  }, [debouncedParam]);
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
   return (
     <div>
       <SearchPanel users={users} param={param} setParam={setParam} />
--- jira/src/utils/index.js
+++ jira/src/utils/index.js
@@ -1,3 +1,6 @@
+import {useEffect, useState} from "react";
+
+
 export const isFalsy = (value) => value === 0? false : !value
 export const cleanObject = (object) =>{
 	//不建议改变传输对象
@@ -11,3 +14,24 @@ export const cleanObject = (object) =>{
 	})
 	return result
 }
+export const useMount = (callback) =>{
+	useEffect(() =>{
+		callback()
+	},[])
+}
+export const useDebounce = (value, delay) => {
+	const [debouncedValue, setDebouncedValue] = useState(value)
+	useEffect(()=>{
+		// console.log("value=",value)
+		const  timeout = setTimeout(() => setDebouncedValue(value), delay)
+		return () => clearTimeout(timeout)
+	},[value,delay])
+	// setTimeout(() => setDebouncedValue("1"), 2000)
+	// setTimeout(() => setDebouncedValue("2"), 4000)
+	// console.log("debouncedValue=",debouncedValue)
+	//此处返回特殊
+	return debouncedValue
+}
+//                首次return d -> const
+//此useEffect程序 第一次执行return d -> return() -> const -> return d(由于debouncesValue变化所以返回)
+//               第二次执行return d -> return() -> const -> return d  ....
